d395a0d7522994f7d11a8ac3ddf7e709077c36af
Ryan Luu <ryanluu@gmail.com>
2019-10-15 22:27:29 -0400

    Initial commit of generated files from 'Spring Initializr'.

---
+++ src/main/java/io/github/rluu/airdash/AirdashApplication.java
@@ -0,0 +1,13 @@
+package io.github.rluu.airdash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirdashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirdashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/rluu/airdash/AirdashApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.rluu.airdash;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AirdashApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
